9a4cc7dc3ccc3672b18702616fdcd46e12185c2b
Jade Shearstone <jade.shearstone@43375.auckland.natcoll.ac.nz>
2014-07-07 14:56:31 +1200

    Pipes added

--- main.js
+++ main.js
@@ -4,57 +4,84 @@ var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
 // Creates a new 'main' state that wil contain the game
 var main_state = {
 
+    // Function called first to load all the assets
     preload: function() { 
-		// Function called first to load all the assets
+		
+        // Change the background color of the game
+        this.game.stage.backgroundColor = '#7F7F7F';
+
+        // Load the bird sprite
+        this.game.load.image('bird', 'assets/ball.png'); 
+
+        // Load the pipe sprite
+        this.game.load.image('pipe', 'assets/pipe.png'); 
     },
 
+    // Fuction called after 'preload' to setup the game
     create: function() { 
-    	// Fuction called after 'preload' to setup the game    
+        // Display the bird on the screen - numbers are x/y co-ordinates
+        this.bird = this.game.add.sprite(100, 245, 'bird');
+
+        // Add gravity to the bird to make it fall
+        this.bird.body.gravity.y = 500;  
+
+        // Call the 'jump' function when the spacekey is hit
+        var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        space_key.onDown.add(this.jump, this);    
+
+        // Create a group of pipes
+        this.pipes = game.add.group();  
+        this.pipes.createMultiple(20, 'pipe');  
+
+        // Calls a pipe every 1.5 seconds
+        this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);   
     },
     
+    // Function called 60 times per second
     update: function() {
-		// Function called 60 times per second
-    },
-
-    // Add bird with spacebar jump
-    preload: function() {  
-    // Change the background color of the game
-    this.game.stage.backgroundColor = '#7F7F7F';
-
-    // Load the bird sprite
-    this.game.load.image('bird', 'assets/ball.png'); 
-    },
-
-    create: function() {  
-    // Display the bird on the screen
-    this.bird = this.game.add.sprite(100, 245, 'bird');
-
-    // Add gravity to the bird to make it fall
-    this.bird.body.gravity.y = 1000;  
-
-    // Call the 'jump' function when the spacekey is hit
-    var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    space_key.onDown.add(this.jump, this);     
-},
-
-update: function() {  
-    // If the bird is out of the world (too high or too low), call the 'restart_game' function
-    if (this.bird.inWorld == false)
+        // If the bird is out of the world (too high or too low), call the 'restart_game' function
+        if (this.bird.inWorld == false)
         this.restart_game();
-},
-// Make the bird jump 
-jump: function() {  
-    // Add a vertical velocity to the bird
-    this.bird.body.velocity.y = -350;
-},
+    },
 
-// Restart the game
-restart_game: function() {  
-    // Start the 'main' state, which restarts the game
-    this.game.state.start('main');
-},
+    // Make the bird jump 
+    jump: function() {  
+        // Add a vertical velocity to the bird
+        this.bird.body.velocity.y = -250;
+    },
 
-};
+    // Add a pipe into the game
+    add_one_pipe: function(x, y) {  
+        // Get the first dead pipe of our group
+        var pipe = this.pipes.getFirstDead();
+
+        // Set the new position of the pipe
+        pipe.reset(x, y);
+
+        // Add velocity to the pipe to make it move left
+        pipe.body.velocity.x = -200; 
+
+        // Kill the pipe when it's no longer visible 
+        pipe.outOfBoundsKill = true;
+    },
+
+    // Add a row of pipes into the game
+    add_row_of_pipes: function() {  
+        var hole = Math.floor(Math.random()*5)+1;
+
+        for (var i = 0; i < 8; i++)
+        if (i != hole && i != hole +1) 
+            this.add_one_pipe(400, i*60+10);   
+    },
+
+    // Restart the game
+        restart_game: function() {  
+        this.game.time.events.remove(this.timer);  
+        // Start the 'main' state, which restarts the game
+        this.game.state.start('main');
+    },
+
+    };
 
 // Add and start the 'main' state to start the game
 game.state.add('main', main_state);  
